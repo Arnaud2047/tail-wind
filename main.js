81f8b91557b7dfc18c3b43ec12238b619b384a6f
Arnaud2047 <91453779+Arnaud2047@users.noreply.github.com>
2022-03-16 16:28:18 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+module.exports = {
+    theme: {
+      screens: {
+     
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+    
+      }
+    }
+}
+
+
+function ScrollRight (){
+    document.getElementById('m').scrollLeft += 200;
+};
+function ScrollLeft  (){
+    document.getElementById('m').scrollLeft -= 200;
+};
+
+
+
+
+
